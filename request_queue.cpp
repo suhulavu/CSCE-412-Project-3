b5b2ea8c952369356ec1234d69fff8fb32aa40e4
suhulavu <suhulavu@gmail.com>
2024-10-19 15:03:32 -0500

    generated initial cpp file for request queue

---
+++ request_queue.cpp
@@ -0,0 +1,13 @@
+#include "request_queue.h"
+
+RequestQueue::RequestQueue() {}
+
+void RequestQueue::push(Request req) {
+    queue.push(req);
+}
+
+Request RequestQueue::pop() {
+    Request req = queue.front();
+    queue.pop();
+    return req;
+}
