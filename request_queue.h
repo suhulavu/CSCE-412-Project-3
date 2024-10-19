534a75a2073f4121e7db17b35f9d872f8bb330e3
suhulavu <suhulavu@gmail.com>
2024-10-19 15:01:10 -0500

    generated initial header file for request queue class

---
+++ request_queue.h
@@ -0,0 +1,18 @@
+#ifndef REQUESTQUEUE_H
+#define REQUESTQUEUE_H
+
+#include "request.h"
+#include <queue>
+
+class RequestQueue {
+    private:
+        std::queue<Request> queue;
+
+    public:
+        RequestQueue();
+        void push(Request req);
+        Request pop();
+};
+
+
+#endif
